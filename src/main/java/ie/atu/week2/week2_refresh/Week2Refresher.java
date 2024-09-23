4d64e9836df8e777005adbbffa152188c8c3e733
Olamide Aderogba <G00401971@atu.ie>
2024-09-23 16:04:22 +0100

    Created Week2Refresher Class.

---
+++ src/main/java/ie/atu/week2/week2_refresh/Week2Refresher.java
@@ -0,0 +1,7 @@
+package ie.atu.week2.week2_refresh;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Week2Refresher {
+}
---
+++ src/test/java/ie/atu/week2/week2_refresh/Week2RefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week2.week2_refresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week2RefreshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
