2f5e6dd63ae9789afded717d55d9b93c3de65a3f
Olamide Aderogba <G00401971@atu.ie>
2024-09-23 17:07:33 +0100

    Running and Testing Application on Talend API

---
+++ src/main/java/ie/atu/week2/week2_refresh/Product.java
@@ -0,0 +1,14 @@
+package ie.atu.week2.week2_refresh;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private Long id;
+    private String name;
+    private double price;
+}
--- src/main/java/ie/atu/week2/week2_refresh/Week2Refresher.java
+++ src/main/java/ie/atu/week2/week2_refresh/Week2Refresher.java
@@ -1,7 +1,24 @@
 package ie.atu.week2.week2_refresh;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 @RestController
 public class Week2Refresher {
+   private List<Product> productlist = new ArrayList<Product>();
+
+   @PostMapping("/products")
+   public Product addProduct(@RequestBody Product product){
+
+      productlist.add(product);
+      return product;
+   }
+   @GetMapping("/products")
+   public List<Product> getProductList(){
+      return productlist;
+   }
+
 }
