2d94a1b07a6a46d95b1706dd7a09755de8abdc37
Olamide Aderogba <G00401971@atu.ie>
2024-09-28 19:04:14 +0100

    Modified the ProductController to use dependency injection with the ProductService.

--- src/main/java/ie/atu/week2/week2_refresh/productService.java
+++ src/main/java/ie/atu/week2/week2_refresh/productService.java
@@ -1,7 +1,26 @@
 package ie.atu.week2.week2_refresh;
 
 
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
 public class productService {
+
+    private List<Product> productList = new ArrayList<>();
+
+
+    public List<Product> getAllProducts() {
+        return productList;
+    }
+
+    public Product addProduct(Product product) {
+        productList.add(product);
+        return product;
+    }
+
 
 
 }
