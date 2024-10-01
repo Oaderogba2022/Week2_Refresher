667118005e413b3d973ef33ac9e1c976c695caf4
Olamide Aderogba <G00401971@atu.ie>
2024-10-01 20:33:02 +0100

    Created Interface for Inventory, updated the product controller to communicate with the Inventory microservice, and updated the application properties.

--- src/main/java/ie/atu/week2/week2_refresh/GlobalExceptionHandler.java
+++ src/main/java/ie/atu/week2/week2_refresh/GlobalExceptionHandler.java
@@ -1,16 +1,26 @@
 package ie.atu.week2.week2_refresh;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public String handleValidationExceptions(MethodArgumentNotValidException ex ){
-        return "Error occurred: " + ex.getBindingResult().getFieldError().getDefaultMessage();
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex){
+        Map <String, String> errors = new HashMap<>();
+        for (FieldError error : ex.getBindingResult().getFieldErrors()){
+            String fieldName =error.getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        }
+        return errors;
     }
 }
--- src/main/java/ie/atu/week2/week2_refresh/Week2RefreshApplication.java
+++ src/main/java/ie/atu/week2/week2_refresh/Week2RefreshApplication.java
@@ -2,8 +2,10 @@ package ie.atu.week2.week2_refresh;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class Week2RefreshApplication {
 
     public static void main(String[] args) {
