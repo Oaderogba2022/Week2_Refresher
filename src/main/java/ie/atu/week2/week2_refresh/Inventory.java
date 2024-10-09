0dd2644bc4c33affcc6a56781311f501fbe3137a
Olamide <Aderogba>
2024-10-09 17:55:13 +0100

    Updated week2 refresher for Inventory microservice.

--- src/main/java/ie/atu/week2/week2_refresh/Inventory.java
+++ src/main/java/ie/atu/week2/week2_refresh/Inventory.java
@@ -3,10 +3,12 @@ package ie.atu.week2.week2_refresh;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class InventoryResponse {
+
+public class Inventory {
     private Long productId;
     private int quantity;
 }
--- src/main/java/ie/atu/week2/week2_refresh/Product.java
+++ src/main/java/ie/atu/week2/week2_refresh/Product.java
@@ -8,9 +8,9 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import jakarta.validation.constraints.Positive;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
+//@Data
+//@AllArgsConstructor
+//@NoArgsConstructor
 public class Product {
     private Long id;
 
@@ -20,4 +20,64 @@ public class Product {
 
     @Positive(message = "Price must be a positive number")
     private double price;
+
+    // Constructor
+    public Product(Long id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    // Default constructor
+    public Product() {
+    }
+
+    // Getter for 'id'
+    public Long getId() {
+        return id;
+    }
+
+    // Getter for 'name'
+    public String getName() {
+        return name;
+    }
+
+    // Getter for 'price'
+    public double getPrice() {
+        return price;
+    }
+
+    // Setter for 'id'
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    // Setter for 'name'
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Setter for 'price'
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
